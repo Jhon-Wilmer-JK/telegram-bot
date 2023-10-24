320c6e317122b3a0a6476004e3c417306be372c4
Jhon_Wilmer_JK <123090106+Jhon-Wilmer-JK@users.noreply.github.com>
2023-10-23 21:21:25 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-TELEGRAM_TOKEN = "6664804026:AAG8bALQDBODF5702AUWlUv8MSBXV2gn-So" ##yaichi
-#TELEGRAM_TOKEN = "6314213819:AAEiqZH0ss9-k83VyTbIa49sI5WysOYGHQ8"   ##listmessage
-
+TELEGRAM_TOKEN = "6664804026:AAG8bALQDBODF5702AUWlUv8MSBXV2gn-So"
 MI_CHAT_ID = 2096120369  #este es mi chat_id con el bot
 GRUPO_STAFF = -1001677796993
-CANAL_ID = -1001780591507  #este es el canal_id de pruebas
-CANAL_ID1 = -1001909656171   #este es el canal_id
+CANAL_ID1 = -1001780591507  #este es el canal_id de pruebas
+CANAL_ID = -1001909656171   #este es el canal_id
 CANAL_CHAT_ID = -1001800044087   #este es el canal_chat_id
-MENSAJE_A = "https://t.me/FateSerieSubES/181"
-API_ID = 25202397
-API_HASH = 'e927a6e0833eafa2eb742cebe9232995'
+
