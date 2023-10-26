225db8b2a3d7557a9b3e4b3e52495f9ce348a8a3
Jhon_Wilmer_JK <123090106+Jhon-Wilmer-JK@users.noreply.github.com>
2023-10-25 20:07:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-TELEGRAM_TOKEN = "6664804026:AAG8bALQDBODF5702AUWlUv8MSBXV2gn-So"
-MI_CHAT_ID = 2096120369  #este es mi chat_id con el bot
-GRUPO_STAFF = -1001677796993
-CANAL_ID1 = -1001780591507  #este es el canal_id de pruebas
-CANAL_ID = -1001909656171   #este es el canal_id
-CANAL_CHAT_ID = -1001800044087   #este es el canal_chat_id
+TELEGRAM_TOKEN = ""
+MI_CHAT_ID = 
+GRUPO_STAFF = 
+CANAL_ID1 = 
+CANAL_ID = 
+CANAL_CHAT_ID = 
 
