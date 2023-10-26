2ec9f5a2503f1a5622b225dcf6020fae450ec1e4
Jhon_Wilmer_JK <123090106+Jhon-Wilmer-JK@users.noreply.github.com>
2023-10-25 20:07:51 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,14 +20,7 @@ for key in DIR:   #creamos los directorio definidos
     except:
         pass
 
-'''
-TELEGRAM_TOKEN = "6664804026:AAG8bALQDBODF5702AUWlUv8MSBXV2gn-So"
-MI_CHAT_ID = 2096120369  #este es mi chat_id con el bot
-GRUPO_STAFF = -1001677796993
-CANAL_ID1 = -1001780591507  #este es el canal_id de pruebas
-CANAL_ID = -1001909656171   #este es el canal_id
-CANAL_CHAT_ID = -1001800044087   #este es el canal_chat_id
-'''
+
 #instanciamos el bot de Telegram
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
